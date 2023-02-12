f694239f37ad55a3688f7335f713423b00780e0d
strongest1avenger <114069917+strongest1avenger@users.noreply.github.com>
2023-02-12 10:20:51 +0000

    labeling

--- golf_tourny/src/app/app.component.ts
+++ golf_tourny/src/app/app.component.ts
@@ -1,16 +1,18 @@
+//Imports all the necessary dependencies
 import { Component, OnInit, Input } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
-
+//Relevent component selectors
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  
   player: any;
 
   constructor(private socket: Socket) { }
-
+//Implements the onInit lifecycle hook and socket to inject an instance from the socket serveer
   ngOnInit() {
     this.socket.on('data-update', (data: any) => {
       console.log(data)
--- golf_tourny/src/app/app.module.ts
+++ golf_tourny/src/app/app.module.ts
@@ -1,10 +1,10 @@
-
+//Imorts the necessary dependencies
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { AppComponent } from './app.component';
-
+//Creating an constant open line to the server
 const config: SocketIoConfig = { url: 'https://mst-full-stack-dev-test.herokuapp.com/', options: {} };
 
 @NgModule({
